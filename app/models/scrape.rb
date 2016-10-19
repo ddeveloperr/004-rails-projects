b9fdde2323662e4573ccdd51aef6c64d75afb266
KemalC <ddeveloperr@gmail.com>
2016-10-19 08:01:53 +0000

    Scraping movies via console - small issue

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -5,18 +5,42 @@ class Scrape
 	def scrape_new_movie
 		begin
 			doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/the_martian/"))
-			doc.css('script').remove 
-			self.title = doc.at("//h1").text.strip  
-			self.hotness = doc.at_css('span.meter-value').text[0, doc.at_css('span.meter-value').text.index('%')] 
-			self.image_url = doc.at_css('#movie-image-section img')['src'] 
+			doc.css('script').remove
+			self.title = doc.at("//h1").text.strip
+			self.hotness = doc.at_css('span.meter-value').text[0, doc.at_css('span.meter-value').text.index('%')]
+			self.image_url = doc.at_css('#movie-image-section img')['src']
 			self.rating = doc.at_css("div.info > div:nth-child(2)").text
-			self.director = doc.at_css("div.info > div:nth-child(6)").text.strip 
+			self.director = doc.at_css("div.info > div:nth-child(6)").text.strip
 			self.genre = doc.at_css("div.info > div:nth-child(4)").text.strip
 			self.runtime = doc.at_css("div.info > div:nth-child(12)").text.strip
-			self.synopsis = doc.css('#movieSynopsis').text.strip
+			s = doc.css('#movieSynopsis').text.strip
+			
+			if ! s.valid_encoding?
+			  s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
+			end
+			
+			self.synopsis = s
+			
 			return true
 		rescue Exception => e
 			self.failure = "Something went wrong with the scrape"
 		end
 	end
+	
+	
+	def save_movie
+	  movie = Movie.new(
+		title: self.title,
+		hotness: self.hotness,
+		image_url: self.image_url,
+		synopsis: self.synopsis,
+		rating: self.rating,
+		genre: self.genre,
+		director: self.director,
+		runtime: self.runtime
+		)
+		
+	  movie.save
+	end
+	
 end
